cbd3b4b05c77bfd536e41dc791a2558b52ef54e8
Mikhail Kolesnikov <dadyaaya@gmail.com>
2022-12-26 13:59:29 +0400

    use simple model

--- models/user.go
+++ models/user.go
@@ -16,21 +16,21 @@ import (
 */
 
 type User struct {
-	id        uuid.UUID
-	name      string
-	login     string
-	password  string
-	phone     string
-	birthDate time.Time
+	id   uuid.UUID
+	name string
+	// login     string
+	// password  string
+	// phone     string
+	// birthDate time.Time
 }
 
 func NewUser(name, login, password, phone string, date time.Time) *User {
 	return &User{
-		id:        uuid.New(),
-		name:      name,
-		login:     login,
-		password:  password,
-		phone:     phone,
-		birthDate: date,
+		id:   uuid.New(),
+		name: name,
+		// login:     login,
+		// password:  password,
+		// phone:     phone,
+		// birthDate: date,
 	}
 }
