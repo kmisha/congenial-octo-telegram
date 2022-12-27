0f6e8cb56bc83dae8f4f9ca7b2f24b5627f8174c
Mikhail Kolesnikov <dadyaaya@gmail.com>
2022-12-27 10:25:59 +0400

    update user model

--- models/user.go
+++ models/user.go
@@ -16,21 +16,29 @@ import (
 */
 
 type User struct {
-	id   uuid.UUID
-	name string
-	// login     string
-	// password  string
-	// phone     string
-	// birthDate time.Time
+	ID        uuid.UUID `json:"id"`
+	Name      string    `json:"name"`
+	Login     string    `json:"login"`
+	Password  string    `json:"password"`
+	Phone     string    `json:"phone"`
+	BirthDate time.Time `json:"birthDate"`
 }
 
 func NewUser(name, login, password, phone string, date time.Time) *User {
 	return &User{
-		id:   uuid.New(),
-		name: name,
-		// login:     login,
-		// password:  password,
-		// phone:     phone,
-		// birthDate: date,
+		ID:        uuid.New(),
+		Name:      name,
+		Login:     login,
+		Password:  password,
+		Phone:     phone,
+		BirthDate: date,
 	}
+}
+
+func (u *User) Patch(other *User) {
+	u.Name = other.Name
+	u.Login = other.Login
+	u.Password = other.Password
+	u.Phone = other.Phone
+	u.BirthDate = other.BirthDate
 }
